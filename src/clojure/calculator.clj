fa0a6e0ef4a13e5a0065bf987755f65ca5b67944
Kevin Krause <kkrause2@carthage.edu>
2019-04-19 18:37:35 -0600

    Update calculator.clj

--- src/clojure/calculator.clj
+++ src/clojure/calculator.clj
@@ -1,3 +1,4 @@
+"Namespace"
 (ns clojure.calculator)
 
 "Add many numbers together"
@@ -8,5 +9,20 @@
 (defn muln [nums]
   (apply * nums))
 
+"Divide two numbers"
+(defn div [num denom]
+  (/ num denom))
+
+"Do some basic math ..."
 (prn "Addition of n numbers:" (addn [1 2]))
 (prn "Multiplication of n numbers:" (muln [5 4 5]))
+(prn "Divide two numbers:" (div 4 2))
+
+"Try using a higher order function"
+(defn addone [num]
+  (+ num 1))
+
+(defn incn [nums]
+  (map addone nums))
+
+(prn "Add 1 to these numbers:" (incn [1 2 3]))
